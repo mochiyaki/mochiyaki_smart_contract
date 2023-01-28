69b2b09ac30e64b4b7abb5427b5a55640a75231f
mochiyaki <123602593+mochiyaki@users.noreply.github.com>
2023-01-28 10:48:35 +0800

    Create base64.sol

---
+++ base64.sol
@@ -0,0 +1,75 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.17;
+
+library Base64 {
+
+    bytes constant private base64stdchars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+    bytes constant private base64urlchars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_=";
+                                            
+    function encode(string memory _str) internal pure returns (string memory) {
+        uint i = 0;
+        uint j = 0;
+
+        uint padlen = bytes(_str).length;
+        if (padlen%3 != 0) padlen+=(3-(padlen%3));
+
+        bytes memory _bs = bytes(_str);
+        bytes memory _ms = new bytes(padlen);
+        
+        for (i=0; i<_bs.length; i++) {
+            _ms[i] = _bs[i];
+        }
+ 
+        uint res_length = (padlen/3) * 4;
+        bytes memory res = new bytes(res_length);
+
+        for (i=0; i < padlen; i+=3) {
+            uint c0 = uint(uint8(_ms[i])) >> 2;
+            uint c1 = (uint(uint8(_ms[i])) & 3) << 4 |  uint(uint8(_ms[i+1])) >> 4;
+            uint c2 = (uint(uint8(_ms[i+1])) & 15) << 2 | uint(uint8(_ms[i+2])) >> 6;
+            uint c3 = (uint(uint8(_ms[i+2])) & 63);
+
+            res[j]   = base64urlchars[c0];
+            res[j+1] = base64urlchars[c1];
+            res[j+2] = base64urlchars[c2];
+            res[j+3] = base64urlchars[c3];
+
+            j += 4;
+        }
+
+        // Adjust trailing empty values
+        if ((padlen - bytes(_str).length) >= 1) { res[j-1] = base64urlchars[64];}
+        if ((padlen - bytes(_str).length) >= 2) { res[j-2] = base64urlchars[64];}
+        return string(res);
+    }
+
+
+    function decode(string memory _str) internal pure returns (string memory) {
+        require( (bytes(_str).length % 4) == 0, "Length not multiple of 4");
+        bytes memory _bs = bytes(_str);
+
+        uint i = 0;
+        uint j = 0;
+        uint dec_length = (_bs.length/4) * 3;
+        bytes memory dec = new bytes(dec_length);
+
+        for (; i< _bs.length; i+=4 ) {
+            (dec[j], dec[j+1], dec[j+2]) = dencode4(
+                bytes1(_bs[i]),
+                bytes1(_bs[i+1]),
+                bytes1(_bs[i+2]),
+                bytes1(_bs[i+3])
+            );
+            j += 3;
+        }
+        while (dec[--j]==0)
+            {}
+
+        bytes memory res = new bytes(j+1);
+        for (i=0; i<=j;i++)
+            res[i] = dec[i];
+
+        return string(res);
+    }
+}
